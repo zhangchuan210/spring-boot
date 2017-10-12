04706730c5a4b5b6f52c34db84f1e97be573af7b
zhangchuan210 <zhangchuan210@163.com>
2017-10-12 14:51:00 +0800

    mybatis

---
+++ spring-boot-mybatis-xml/src/main/java/cn/cdboost/domain/MenuAction.java
@@ -0,0 +1,52 @@
+package cn.cdboost.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MenuAction implements Serializable {
+    private static final long serialVersionUID = 7247714666080613214L;
+    private Long actionID;
+    private Long menuID;
+    private String actionName;
+    private Date createTime;
+    private Long createUserID;
+
+    public Long getActionID() {
+        return actionID;
+    }
+    public void setActionID(Long actionID) {
+        this.actionID = actionID;
+    }
+    public String getActionName() {
+        return actionName;
+    }
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
+    }
+
+    public Long getMenuID() {
+        return menuID;
+    }
+    public void setMenuID(Long menuID) {
+        this.menuID = menuID;
+    }
+    public Date getCreateTime() {
+        return createTime;
+    }
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+    public Long getCreateUserID() {
+        return createUserID;
+    }
+    public void setCreateUserID(Long createUserID) {
+        this.createUserID = createUserID;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuAction [actionID=" + actionID + ", actionName=" + actionName + ", menuID=" + menuID
+                + ", createTime=" + createTime + ", createUserID=" + createUserID + "]";
+    }
+
+}
