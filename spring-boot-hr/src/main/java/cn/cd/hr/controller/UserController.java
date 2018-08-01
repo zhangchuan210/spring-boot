d826c527d20d28cfa0a6fee725fe2428539aadeb
zhangchuan <zhangchuan210@163.com>
2018-08-01 15:44:26 +0800

    a

--- spring-boot-hr/src/main/java/cn/cd/hr/controller/UserController.java
+++ spring-boot-hr/src/main/java/cn/cd/hr/controller/UserController.java
@@ -5,12 +5,17 @@ import cn.cd.hr.mapper.UserMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -23,11 +28,35 @@ public class UserController {
         user.setIdCardNo("123");
         user.setUserName("aaa");
         user.setTel("123");
-        userMapper.insert(user);
+        //userMapper.insert(user);
 
         User user1 = userMapper.getByIdCardNo("123");
         return "Hello World";
     }
 
+    @RequestMapping("/add")
+    public String add() {
+        return "add";
+    }
 
+    @RequestMapping("/query")
+    public String query() {
+        return "query";
+    }
+
+    @RequestMapping("/addUser")
+    public String addUser(@Valid User user, BindingResult result, Model model) {
+
+        userMapper.insert(user);
+
+        return "SUCCESS";
+    }
+
+    @RequestMapping("/queryUser")
+    public Model queryUser(@Valid User user, BindingResult result, Model model) {
+
+        List<User> list = userMapper.queryUsers(user);
+        model.addAttribute("users", list);
+        return model;
+    }
 }
