5fba05fd7c68d389b8f6887f1149be792dd00f64
zhangchuan <zhangchuan210@163.com>
2018-08-13 17:29:10 +0800

    a

--- spring-boot-hr/src/main/java/cn/cd/hr/controller/UserController.java
+++ spring-boot-hr/src/main/java/cn/cd/hr/controller/UserController.java
@@ -2,18 +2,25 @@ package cn.cd.hr.controller;
 
 import cn.cd.hr.entity.User;
 import cn.cd.hr.mapper.UserMapper;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -63,5 +70,43 @@ public class UserController {
         model.addAttribute("users", list);
 
         return list;
+    }
+
+    @RequestMapping("/outPut")
+    public String outPut(@Valid User user) throws IOException {
+
+        List<User> list = userMapper.queryUsers(user);
+
+        this.downLoad(list);
+
+        return "success";
+    }
+
+    private void downLoad(List<User> list) throws IOException {
+        if(!CollectionUtils.isEmpty(list)){
+            // 建立一个Excel
+            Workbook book = new HSSFWorkbook();
+            // 在对应的Excel中建立一个分表
+            Sheet sheet1 =(Sheet) book.createSheet("表1");
+            int i = 0;
+            for (User user:list) {
+                // 设置相应的行（初始从0开始）
+                Row row =sheet1.createRow(i);
+
+                Cell cell1 = row.createCell(0);
+                cell1.setCellValue(user.getUserName());
+
+                Cell cell2 = row.createCell(1);
+                cell2.setCellValue(user.getSex());
+
+                Cell cell3 = row.createCell(2);
+                cell3.setCellValue(user.getTel());
+
+                i++;
+            }
+            // 保存到计算机相应路径
+            book.write( new FileOutputStream("D://员工表.xls"));
+
+        }
     }
 }
